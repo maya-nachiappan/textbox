79d378e29b2480292978b4b00daf0089c4a347c4
maya-nachiappan <maya.n102304@gmail.com>
2024-11-22 19:18:12 -0800

    created main, header and cpp files. created class called text box with private attribute called value (string), default constructor and second constructor using explicit, wrapped inside UIComponents namespace.

---
+++ textbox/textbox.cpp
@@ -0,0 +1,8 @@
+//
+//  textbox.cpp
+//  textbox
+//
+//  Created by maya nachiappan on 11/22/24.
+//
+
+#include "textbox.hpp"
---
+++ textbox/textbox.hpp
@@ -0,0 +1,13 @@
+//
+//  textbox.hpp
+//  textbox
+//
+//  Created by maya nachiappan on 11/22/24.
+//
+
+#ifndef textbox_hpp
+#define textbox_hpp
+
+#include <stdio.h>
+
+#endif /* textbox_hpp */
